76101f34f1506ba3dfaeeb61133c5028330d93d0
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-04-11 08:11:21 +0600

    update

--- src/components/Sidebar/SidebarMenu.tsx
+++ src/components/Sidebar/SidebarMenu.tsx
@@ -2,12 +2,8 @@ import styles from './SidebarMenu.module.css';
 import SidebarMenuItem from './SidebarMenuItem';
 
 const menuItems = [
-  { name: 'dashboard', icon: 'bx bx-grid-alt', text: 'Dashboard', path: '/dashboard' },
-  { name: 'home', icon: 'bx bx-home', text: 'Home', path: '/home' },
-  { name: 'order', icon: 'bx bx-bar-chart-alt-2', text: 'Order', path: '/order' },
-  { name: 'messages', icon: 'bx bx-message', text: 'Messages', path: '/messages' },
-  { name: 'favorites', icon: 'bx bx-heart', text: 'Favorites', path: '/favorites' },
-  { name: 'settings', icon: 'bx bx-cog', text: 'Settings', path: '/settings' },
+  { name: 'dashboard', icon: 'bx bx-grid-alt', text: 'Dashboard', path: '/dashboard' },  
+  { name: 'order', icon: 'bx bx-bar-chart-alt-2', text: 'Order', path: '/order' },  
   { name: 'login', icon: 'bx bx-log-in', text: 'Login', path: '/login' },
   { name: 'signup', icon: 'bx bx-user-plus', text: 'Signup', path: '/signup' },
 ];
